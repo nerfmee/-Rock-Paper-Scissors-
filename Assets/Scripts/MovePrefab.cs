47922734512741a159374e1b9e4fcc2f9312dcfa
nerfmee <41577753+nerfmee@users.noreply.github.com>
2020-06-20 11:29:56 +0300

    AddProject

---
+++ Assets/Scripts/MovePrefab.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MovePrefab : MonoBehaviour
+{
+    public float moveSpeed;
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * moveSpeed);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] _prefabs;
+
+
+    public void Spawn()
+    {
+        int RandomPrefab = Random.Range(0, _prefabs.Length);
+
+        Instantiate(_prefabs[RandomPrefab],transform.position,transform.rotation);
+    }
+
+    private IEnumerator SpawnPerTime()
+    {
+        Spawn();
+        yield return new WaitForSeconds(2);
+        StartCoroutine(SpawnPerTime());
+    }
+
+
+    private void Start()
+    {
+        StartCoroutine(SpawnPerTime());
+    }
+}
