22148fcdc45ad1dc416b7e8a1ec37b83318337c8
nerfmee <41577753+nerfmee@users.noreply.github.com>
2020-06-23 20:16:14 +0300

    UpdateStartScene

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,8 +7,6 @@ public class StartMenu : MonoBehaviour
 {
     public void HideStartMenu()
     {
-        Debug.Log("hi");
-
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 }
